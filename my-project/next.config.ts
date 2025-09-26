c9d0b5d783ec7ba115f7bfad382fb99ed7206335
P4cho <jakubpaszko01@gmail.com>
2025-09-26 16:48:54 +0200

    module

--- my-project/next.config.ts
+++ my-project/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  module.exports = {
+  async redirects() {
+    return [{ source: '/', destination: '/app', permanent: false }];
+  }
+}
 };
 
 export default nextConfig;
